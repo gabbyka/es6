c6517286a2bcb6b62ae921346f6096f2261c0bdf
gn <gn@star-trac.de>
2018-07-17 17:00:33 +0200

    added webpack dev server

--- build/bundle.js
+++ build/bundle.js
@@ -81,19 +81,19 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/index.js":
+/***/ "./app/index.js":
 /*!**********************!*\
-  !*** ./src/index.js ***!
+  !*** ./app/index.js ***!
   \**********************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("console.log('hello webpack');\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("console.log('hello webpack');\n\n//# sourceURL=webpack:///./app/index.js?");
 
 /***/ })
 
